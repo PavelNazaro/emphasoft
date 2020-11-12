69b9efedfc5f3155d96cf666ff802e7cf3ecb4dc
pavelnazaro <pavelnazaro@mail.ru>
2020-11-12 22:50:54 +0300

    Initial commit

---
+++ src/main/java/com/pavelnazaro/emphasoft/EmphasoftApplication.java
@@ -0,0 +1,13 @@
+package com.pavelnazaro.emphasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmphasoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmphasoftApplication.class, args);
+    }
+
+}
